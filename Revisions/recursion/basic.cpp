492b3880202f48a29f78bf248c8ffa322f8c7315
Rishav Kumar <rishavkumar18902@gmail.com>
2025-04-27 14:50:37 +0530

    basics of recursion

--- Revisions/recursion/basic.cpp
+++ Revisions/recursion/basic.cpp
@@ -1,25 +1,68 @@
-// print 1 to N using recursion 
+// print 1 to N using recursion
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve(int n){
+//     if(n == 0){
+//         return ;
+//     }
+
+//     solve(n - 1);
+
+//     cout<< n << endl;
+
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     solve(n);
+// }
 
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve(int n){
-    if(n == 0){
-        return ;
-    }
-
-    solve(n - 1);
-
-    cout<< n << endl;
-
-}
-
-int main(){
-    int n;
-    cin >> n;
-    solve(n);
-}
 
 
+// print N to 1 using recurison
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve(int n)
+// {
+//     if (n == 0)
+//     {
+//         return;
+//     }
+
+//     cout << n << endl;
+
+//     solve(n - 1);
+
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     solve(n);
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int n){
+//     if(n == 0 || n == 1){
+//         return 1;
+//     }
+//     return n * solve(n - 1);
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     int ans = solve(n);
+//     cout << ans;
+// }
---
+++ Revisions/recursion/fibb.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibb(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+
+    int ans =  fibb(n - 1) + fibb(n - 2);
+    return ans;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fibb(n);
+}
