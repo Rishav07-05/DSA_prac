0c3a51e826462f13afd1b719f7405214ce4e8b25
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-14 01:30:17 +0530

    pattern 9;

--- Revisions/Loops/allPattern.cpp
+++ Revisions/Loops/allPattern.cpp
@@ -99,31 +99,76 @@
 
 // pattern 7
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++){
+//         // space
+
+//         for (int j = 0; j < n - i - 1; j++){
+//             cout << " ";
+//         }
+        
+//         // star
+        
+//         for (int j = 0; j < i * 2 + 1; j++)
+//         {
+//             cout << "*";
+//         }
+
+        
+//         // space 
+
+//         for (int j = 0; j < n - i - 1; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+// pattern 8
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i = n - 1; i >= 0; i--){
+//         for (int j = 0; j < n - i - 1; j++){
+//             cout << " ";
+//         }
+//         for (int j = 0; j < i * 2 + 1; j++){
+//             cout << "*";
+//         }
+//         for (int j = 0; j < n - i - 1; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// pattern 9
+
 #include <iostream>
 using namespace std;
 int main(){
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++){
-        // space
-
-        for (int j = 0; j < n - i - 1; j++){
-            cout << " ";
-        }
-        
-        // star
-        
-        for (int j = 0; j < i * 2 + 1; j++)
-        {
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        int star = i;
+        if(i > n)
+            star = 2 * n - i;
+        for (int j = 1; j <= star; j++){
             cout << "*";
-        }
-
-        
-        // space 
-
-        for (int j = 0; j < n - i - 1; j++){
-            cout << " ";
         }
         cout << endl;
     }
