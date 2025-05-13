d8d063b783a07726d06af6b5dcb697210f0e4a28
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-13 22:14:48 +0530

    pattern 3

--- Revisions/Loops/allPattern.cpp
+++ Revisions/Loops/allPattern.cpp
@@ -16,14 +16,31 @@
 
 // pattern 2
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++){
+//         for (int j = 0; j < i + 1; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// pattern 3
+
 #include <iostream>
 using namespace std;
 int main(){
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < i + 1; j++){
-            cout << "* ";
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
         }
         cout << endl;
     }
