3b9479a6be32af5b3be60fcdf8416c3a78663f7d
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-14 12:28:23 +0530

    pattern 13

--- Revisions/Loops/allPattern.cpp
+++ Revisions/Loops/allPattern.cpp
@@ -228,17 +228,33 @@
 
 // pattern 12
 
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
+//     int num = 1;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= i; j++){
+//             cout << num << " ";
+//             num += 1;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// pattern 13
+
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
     int n;
     cin >> n;
-    int num = 1;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= i; j++){
-            cout << num << " ";
-            num += 1;
+    for (int i = 0; i < n; i++){
+        for (char j = 'A'; j <= 'A' + i; j++){
+            cout << j << " ";
         }
         cout << endl;
     }
