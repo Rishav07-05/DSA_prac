d4e0b5877b194a099391d0d31bac5b811ad84438
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-14 12:25:46 +0530

    pattern 12

--- Revisions/Loops/allPattern.cpp
+++ Revisions/Loops/allPattern.cpp
@@ -200,27 +200,46 @@
 
 // pattern 11
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int space = 2 * (n - 1);
+//     for (int i = 1; i <= n; i++)
+//     {
+
+//         for (int j = 1; j <= i; j++){
+//             cout << j;
+//         }
+
+//         for (int j = 1; j <= space; j++){
+//             cout << " ";
+//         }
+
+//         for (int j = i; j >= 1; j--){
+//             cout << j;
+//         }
+//         cout << endl;
+//         space -= 2;
+//     }
+// }
+
+
+// pattern 12
+
 #include <iostream>
 using namespace std;
-int main(){
+int main() {
     int n;
     cin >> n;
-    int space = 2 * (n - 1);
+    int num = 1;
     for (int i = 1; i <= n; i++)
     {
-
         for (int j = 1; j <= i; j++){
-            cout << j;
-        }
-
-        for (int j = 1; j <= space; j++){
-            cout << " ";
-        }
-
-        for (int j = i; j >= 1; j--){
-            cout << j;
+            cout << num << " ";
+            num += 1;
         }
         cout << endl;
-        space -= 2;
     }
 }
