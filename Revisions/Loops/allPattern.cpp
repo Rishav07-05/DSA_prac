af3e57f49811cc8c26d8e53e12be205dcf0cabab
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-14 19:14:38 +0530

    file modification

--- Revisions/Loops/allPattern.cpp
+++ Revisions/Loops/allPattern.cpp
@@ -279,15 +279,17 @@
 
 // pattern 15
 
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        for (char j = 'A'; j <= 'A' + i; j++){
-            cout << char('A' + i) << " ";
-        }
-        cout << endl;
-    }
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++){
+//         for (char j = 'A'; j <= 'A' + i; j++){
+//             cout << char('A' + i) << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
