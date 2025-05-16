44b7ea3b352e6920ff783b85d06ced21a62ad3d8
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-17 00:15:12 +0530

    longest subarray using two pointer approach

--- Revisions/array/longestSubArray.cpp
+++ Revisions/array/longestSubArray.cpp
@@ -31,24 +31,49 @@ int main(){
 
     // hashing approach
 
-    unordered_map<int, int> prefixmap;
-    int sum = 0, maxLen = 0;
-    for (int i = 0; i < n; i++){
+    // unordered_map<int, int> prefixmap;
+    // int sum = 0, maxLen = 0;
+    // for (int i = 0; i < n; i++){
 
-        sum += arr[i];
+    //     sum += arr[i];
+
+    //     if(sum == k){
+    //         maxLen = i + 1;
+    //     }
+
+    //     if(prefixmap.find(sum - k) != prefixmap.end()){
+    //         int prevIdx = prefixmap[sum - k];
+    //         maxLen = max(maxLen, i - prevIdx);
+    //     }
+
+    //     if(prefixmap.find(sum) == prefixmap.end()){
+    //         prefixmap[sum] = i;
+    //     }
+    // }
+    // cout << maxLen << " ";
+
+
+
+    // two pointer approach  (sliding window)
+
+    int i = 0, j = 0;
+    int sum = 0;
+    int len = 0;
+
+    while(j < arr.size()){
+
+        sum += arr[j];
+
+        while(sum > k && i <= j){
+            sum -= arr[i];
+            i++;
+        }
 
         if(sum == k){
-            maxLen = i + 1;
+            len = max(len, j - i + 1);
         }
-
-        if(prefixmap.find(sum - k) != prefixmap.end()){
-            int prevIdx = prefixmap[sum - k];
-            maxLen = max(maxLen, i - prevIdx);
-        }
-
-        if(prefixmap.find(sum) == prefixmap.end()){
-            prefixmap[sum] = i;
-        }
+        j++;
     }
-    cout << maxLen << " ";
+    cout<<  len;
 }
+
