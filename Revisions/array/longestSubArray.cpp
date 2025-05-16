5db8368de1871993e6c26ae1323002642f1a66a9
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-16 21:43:22 +0530

    longest subarray with hashing approach

--- Revisions/array/longestSubArray.cpp
+++ Revisions/array/longestSubArray.cpp
@@ -13,20 +13,42 @@ int main(){
 
     // brute force 
 
-    int len = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int sum = 0;
-        for (int j = i; j < n; j++)
-        {
-            sum += arr[j];
-            if(sum == k){
-                len = max(len, j - i + 1);
-            }
+    // int len = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int sum = 0;
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         sum += arr[j];
+    //         if(sum == k){
+    //             len = max(len, j - i + 1);
+    //         }
+    //     }
+    // }
+    // cout << len;
+
+
+
+    // hashing approach
+
+    unordered_map<int, int> prefixmap;
+    int sum = 0, maxLen = 0;
+    for (int i = 0; i < n; i++){
+
+        sum += arr[i];
+
+        if(sum == k){
+            maxLen = i + 1;
+        }
+
+        if(prefixmap.find(sum - k) != prefixmap.end()){
+            int prevIdx = prefixmap[sum - k];
+            maxLen = max(maxLen, i - prevIdx);
+        }
+
+        if(prefixmap.find(sum) == prefixmap.end()){
+            prefixmap[sum] = i;
         }
     }
-    cout << len;
-
-
-
+    cout << maxLen << " ";
 }
