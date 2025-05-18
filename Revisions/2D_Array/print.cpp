6b8ebd99d6bf70133df2e342fa7d41507a1829e0
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-18 17:11:07 +0530

    basic of 2D matrix

---
+++ Revisions/2D_Array/print.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int row, col;
+    cin >> row >> col;
+    vector<vector<int>> arr(row , vector<int>(col));
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++){
+            cout<< arr[i][j];
+        }
+        cout << endl;
+    }
+}
