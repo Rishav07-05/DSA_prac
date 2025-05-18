cb37e16ee23a9070f2c7645d9de4904a7fefa535
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-18 09:08:14 +0530

    two sum with hashing approach

--- Revisions/array/twoSum.cpp
+++ Revisions/array/twoSum.cpp
@@ -10,21 +10,42 @@ int main(){
     int tar;
     cin >> tar;
 
+
+    // brute force 
+
+    // pair<int, int> ans = {-1 , -1};
+
+    // for (int i = 0; i < n; i++){
+    //     for (int j = i + 1; j < n; j++){
+    //         if(arr[i] + arr[j] == tar){
+    //             ans = {i, j};
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // if(ans.first != -1){
+    //     cout << ans.first << " " << ans.second;
+    // }
+    // else{
+    //     cout << "not found";
+    // }
+
+
+
+    // hashmap approach
+
+    unordered_map<int, int> prefixMap;
+
     pair<int, int> ans = {-1 , -1};
 
-    for (int i = 0; i < n; i++){
-        for (int j = i + 1; j < n; j++){
-            if(arr[i] + arr[j] == tar){
-                ans = {i, j};
-                break;
-            }
+    for (int i = 0; i < n; i++)
+    {
+        int totalNum = tar - arr[i];
+        if(prefixMap.find(totalNum) != prefixMap.end()){
+            ans = {prefixMap[totalNum], i};
         }
+        prefixMap[arr[i]] = i;
     }
-
-    if(ans.first != -1){
-        cout << ans.first << " " << ans.second;
-    }
-    else{
-        cout << "not found";
-    }
+    cout << ans.first << " " << ans.second;
 }
