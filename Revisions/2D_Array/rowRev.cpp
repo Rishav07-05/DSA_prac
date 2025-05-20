f3cbfdf7ba06b9d04fb21bfe9f6a48d84e1df959
Rishav Kumar <rishavkumar18902@gmail.com>
2025-05-20 12:38:50 +0530

    reversing the rows

---
+++ Revisions/2D_Array/rowRev.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int row, col;
+    cin >> row >> col;
+    vector<vector<int>> arr(row, vector<int> (col));
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    // for (int i = 0; i < row; i++){
+    //     for (int j = col - 1; j >= 0; j--){
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < row; i++){
+        reverse(arr[i].begin(), arr[i].end());
+    }
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+}   
