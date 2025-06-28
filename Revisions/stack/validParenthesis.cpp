33f76d7b9ead4c192c550ce45bd26086dccb99e1
Rishav Kumar <rishavkumar18902@gmail.com>
2025-06-28 06:58:33 +0530

    valid parenthesis using stack

---
+++ Revisions/stack/validParenthesis.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkParenthesis(stack<char> st , string &str){
+
+    for (int i = 0; i < str.length(); i++){
+        
+        if (str[i] == '('  || str[i] == '[' || str[i] == '{' )
+        {
+            st.push(str[i]);
+        }
+
+        else
+        {
+            if(!st.empty())
+            {
+                if ((st.top() == '(' && str[i] == ')') || (st.top() == '{' && str[i] == '}') || (st.top() == '[' && str[i] == ']'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main(){
+    string str;
+    cin >> str;
+    stack<char> st;
+
+    if (checkParenthesis(st , str)){
+        cout << "Is valid Parenthesis";
+    }
+    else{
+        cout << "Is not valid Parenthesis";
+    }
+}
