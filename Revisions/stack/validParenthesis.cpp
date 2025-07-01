2f0407a428f1ebf5a91bdc2f20cd98d6c627b17e
Rishav Kumar <rishavkumar18902@gmail.com>
2025-07-01 22:41:49 +0530

    comments to file

--- Revisions/stack/validParenthesis.cpp
+++ Revisions/stack/validParenthesis.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// check for valid parenthesis 
+
 bool checkParenthesis(stack<char> st , string &str){
 
     for (int i = 0; i < str.length(); i++){
