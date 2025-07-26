f2c53f59a16174884a6e0d91e3cdb0ecd5f0a021
Rishav Kumar <rishavkumar18902@gmail.com>
2025-07-26 10:46:47 +0530

    cycle detection in directed graph

---
+++ Revisions/Graph/detectCyclicDirected.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool detectCycleDfs(unordered_map<int, bool> &visited, unordered_map<int, bool> &dfsVisited, unordered_map<int, list<int>> &adj , int node)
+{
+    visited[node] = true;
+    dfsVisited[node] = true;
+
+    for(auto i: adj[node])
+    {
+        if(!visited[i])
+        {
+            bool ans = detectCycleDfs(visited, dfsVisited, adj, i);
+            if(ans)
+            {
+                return true;
+            }
+        }
+        else if(dfsVisited[i])
+        {
+            return true;
+        }
+    }
+
+    dfsVisited[node] = false;
+    return false;
+}
+
+bool isCyclic(unordered_map<int, list<int>> &adj, int n, int m)
+{
+
+    unordered_map<int, bool> visited , dfsVisited;
+    for (int i = 0; i < n; i++)
+    {
+        if(!visited[i])
+        {
+            bool ans = detectCycleDfs(visited, dfsVisited, adj , i);
+            if(ans)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    vector<vector<int>> edges;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        edges.push_back({u, v});
+    }
+
+    unordered_map<int, list<int>> adj;
+
+    for (int i = 0; i < m; i++)
+    {
+        int src = edges[i][0];
+        int dest = edges[i][1];
+
+        adj[src].push_back(dest);
+    }
+
+    bool ans = isCyclic(adj , n , m);
+
+    if(ans)
+    {
+        cout << "Cycle detected";
+    }
+    else
+    {
+        cout << "Cycle not present";
+    }
+}
